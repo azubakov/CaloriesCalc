edc36db6a40d9c79e7c7c14887d8f593f5717602
azubakov <azubakov@gmail.com>
2016-08-09 18:50:06 +0300

    I changed Activity to CalorieRecycleActivity and I added resultActivity.

---
+++ app/src/main/java/azubakov/edu/caloriescalc/activity/resultActivity.java
@@ -0,0 +1,15 @@
+package azubakov.edu.caloriescalc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import azubakov.edu.caloriescalc.R;
+
+public class resultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
