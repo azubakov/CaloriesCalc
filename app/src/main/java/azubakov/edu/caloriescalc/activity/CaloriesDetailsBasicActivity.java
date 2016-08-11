b41f27a8cf86beec7b7342087eb0065cbd8c0cbf
azubakov <azubakov@gmail.com>
2016-08-11 22:24:21 +0300

    fixing errors.

--- app/src/main/java/azubakov/edu/caloriescalc/activity/CaloriesDetailsBasicActivity.java
+++ app/src/main/java/azubakov/edu/caloriescalc/activity/CaloriesDetailsBasicActivity.java
@@ -3,6 +3,7 @@ package azubakov.edu.caloriescalc.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,6 +40,7 @@ public class CaloriesDetailsBasicActivity extends AppCompatActivity {
 
     Button btnSaveOrUpdate;
     RelativeLayout layer;
+    Toolbar toolbar;
 
     private String id;
 
@@ -49,7 +51,7 @@ public class CaloriesDetailsBasicActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calories_details_basic);
 
-
+        //setSupportActionBar(toolbar);
         //Registration elements
         findViews();
 
@@ -185,6 +187,7 @@ public class CaloriesDetailsBasicActivity extends AppCompatActivity {
 
         layer = (RelativeLayout) findViewById(R.id.layer);
         btnSaveOrUpdate = (Button) findViewById(R.id.btnSaveOrUpdate);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
 
     }
